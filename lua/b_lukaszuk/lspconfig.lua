c77571911c760f63309e82add7fe484de4d14c49
Bartek Lukaszuk <bartek.lukaszuk@o2.pl>
2022-05-11 22:27:05 +0200

    dodano tsserver

--- lua/b_lukaszuk/lspconfig.lua
+++ lua/b_lukaszuk/lspconfig.lua
@@ -1,7 +1,8 @@
 local nvim_lsp = require('lspconfig')
-local servers = { 'metals' }
+local servers = { 'metals', 'tsserver' }
 
 -- metals (lsp for Scala), root_dir ("build.sbt", "build.sc", "build.gradle", "pom.xml")
+-- tsserver (lsp for TypeScript), root_dir ("package.json", "tsconfig.json", "jsconfig.json", ".git")
 
 for _, lsp in ipairs(servers) do
     nvim_lsp[lsp].setup { }
